5dc90645de9e7e85df5f5020b4fc00cee4aee6fa
Samuel Mugo <66863589+Sammienator@users.noreply.github.com>
2023-04-17 17:05:27 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// set your date here
+const deadline = new Date(" JULY 20, 2023 15:37:25").getTime();
+
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let t = deadline - now;
+  const days = Math.floor(t / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((t % (1000 * 60)) / 1000);
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = minutes;
+  document.getElementById("second").innerHTML = seconds;
+  if (t < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "TIME UP";
+    document.getElementById("day").innerHTML = "0";
+    document.getElementById("hour").innerHTML = "0";
+    document.getElementById("minute").innerHTML = "0";
+    document.getElementById("second").innerHTML = "0";
+  }
+}, 1000);
